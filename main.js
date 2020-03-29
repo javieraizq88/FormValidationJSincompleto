f4cce3a95c8048c421c62dddb9b97516269e82a6
Javiera Izquierdo M <javieraizquierdo7@gmail.com>
2020-03-29 15:48:13 -0300

    padding entre botones. Listo

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 console.log("form validation");
 
-function validar () {
-    let enviar = true; 
+function validar() {
+    let enviar = true;
 
     let form = document.getElementById("loginForm");
 
@@ -11,78 +11,78 @@ function validar () {
 
     console.log(Card);*/
 
-    if (!form.Card.value.trim().length > 0){
+    if (!form.Card.value.trim().length > 0) {
         enviar = false;
-        form.Card.focus ();
-        form.Card.classList.add("is-invalid");
+        form.Card.focus();
+        form.Card.classList.add("bd-danger");
     } else {
-        form.Card.classList.remove("is-invalid");
+        form.Card.classList.remove("bd-danger");
         form.Card.classList.add("is-valid");
     }
 
-    if (!form.CVC.value.trim().length > 0){
+    if (!form.CVC.value.trim().length > 0) {
         enviar = false;
-        form.CVC.focus ();
-        form.CVC.classList.add("is-invalid");
+        form.CVC.focus();
+        form.CVC.classList.add("bd-danger");
     } else {
-        form.CVC.classList.remove("is-invalid");
+        form.CVC.classList.remove("bd-danger");
         form.CVC.classList.add("is-valid");
     }
 
-    if (!form.Amount.value.trim().length > 0){
+    if (!form.Amount.value.trim().length > 0) {
         enviar = false;
-        form.Amount.focus ();
-        form.Amount.classList.add("is-invalid");
+        form.Amount.focus();
+        form.Amount.classList.add("bd-danger");
     } else {
-        form.Amount.classList.remove("is-invalid");
+        form.Amount.classList.remove("bd-danger");
         form.Amount.classList.add("is-valid");
     }
-    
-    if (!form.firstName.value.trim().length > 0){
+
+    if (!form.firstName.value.trim().length > 0) {
         enviar = false;
-        form.firstName.focus ();
-        form.firstName.classList.add("is-invalid");
+        form.firstName.focus();
+        form.firstName.classList.add("bd-danger");
     } else {
-        form.firstName.classList.remove("is-invalid");
+        form.firstName.classList.remove("bd-danger");
         form.firstName.classList.add("is-valid");
     }
 
-    if (!form.lastName.value.trim().length > 0){
+    if (!form.lastName.value.trim().length > 0) {
         enviar = false;
-        form.lastName.focus ();
-        form.lastName.classList.add("is-invalid");
+        form.lastName.focus();
+        form.lastName.classList.add("bd-danger");
     } else {
-        form.lastName.classList.remove("is-invalid");
+        form.lastName.classList.remove("bd-danger");
         form.lastName.classList.add("is-valid");
     }
 
-    if (!form.City.value.trim().length > 0){
+    if (!form.City.value.trim().length > 0) {
         enviar = false;
-        form.City.focus ();
-        form.City.classList.add("is-invalid");
+        form.City.focus();
+        form.City.classList.add("bd-danger");
     } else {
-        form.City.classList.remove("is-invalid");
+        form.City.classList.remove("bd-danger");
         form.City.classList.add("is-valid");
     }
 
-    if (!form.inputState.value.trim().length > 0){
+    if (!form.inputState.value.trim().length > 0) {
         enviar = false;
-        form.inputState.focus ();
-        form.inputState.classList.add("is-invalid");
+        form.inputState.focus();
+        form.inputState.classList.add("bd-danger");
     } else {
-        form.inputState.classList.remove("is-invalid");
+        form.inputState.classList.remove("bd-danger");
         form.inputState.classList.add("is-valid");
     }
 
-    if (!form.postalCode.value.trim().length > 0){
+    if (!form.postalCode.value.trim().length > 0) {
         enviar = false;
-        form.postalCode.focus ();
-        form.postalCode.classList.add("is-invalid");
+        form.postalCode.focus();
+        form.postalCode.classList.add("bd-danger");
     } else {
-        form.postalCode.classList.remove("is-invalid");
+        form.postalCode.classList.remove("bd-danger");
         form.postalCode.classList.add("is-valid");
     }
-    
+
 
     return enviar;
 }
